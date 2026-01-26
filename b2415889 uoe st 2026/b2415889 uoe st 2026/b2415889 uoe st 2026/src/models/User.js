bed1799d2db76ef9a1c3dfebb5c13a1499e09625
Cx330-code <148308791+Cx330-code@users.noreply.github.com>
2026-01-27 00:59:53 +0300

    Delete b2415889 uoe st 2026/b2415889 uoe st 2026/b2415889 uoe st 2026 directory
    merging and arranging the files

--- b2415889 uoe st 2026/b2415889 uoe st 2026/b2415889 uoe st 2026/src/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const chatMessageSchema = new mongoose.Schema({
-  roomId: { type: String, required: true },
-  sender: { type: String, required: true },
-  message: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
-  readBy: { type: [String], default: [] } // Array of user IDs who read the message
-});
